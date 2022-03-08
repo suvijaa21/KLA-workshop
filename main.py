199d4ef8c242c19fb7291f88fffb502b9a389399
Suvijaa shri <85983700+suvijaa21@users.noreply.github.com>
2022-03-08 12:07:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import datetime
+now = datetime.datetime.now()
+print (now.strftime("%Y-%m-%d %H:%M:%S"),end="")
+print("000000")
+import yaml
+
+with open(r'C:\Users\DELL\Downloads\DataSet\Examples\Milestone1\Milestone1_Example.yaml') as file:
+    documents = yaml.full_load(file)
+    # for i in documents.keys():
+    #     print(i)
+    print(documents['M1SampleWorkFlow']['Activities']['TaskA'])
+    f = open("demofile2.txt", "a")
+    while True:
+        line = documents.readline()
+        if not line:
+            break
+        string=now.strftime("%Y-%m-%d %H:%M:%S.")+"000000;"
+        f.write(string)
+    file1.close()
